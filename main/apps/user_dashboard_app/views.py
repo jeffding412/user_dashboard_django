d42027e751309b2be5c8e487d2bc0c2b1c49065f
dingzhengfuaa <dingzhengfuaa@gmail.com>
2018-07-16 19:20:34 -0700

    Login and Registartion validation

--- main/apps/user_dashboard_app/views.py
+++ main/apps/user_dashboard_app/views.py
@@ -13,7 +13,6 @@ def register(request):
     return render(request, "user_dashboard_app/register.html")
 
 def register_user(request):
-
     request.session['errors'] = User.objects.basic_validator(request.POST)
     if len(request.session['errors']):
         # if the errors object contains anything, loop through each key-value pair and make a flash message
@@ -30,14 +29,23 @@ def register_user(request):
     user = User.objects.create(first_name=request.POST['first'],last_name=request.POST['last'],email=request.POST['email'],password_hash=pw_hash,user_level=level,description="")
 
     # redirect to a success route
-    if level == 9:
+    if  level == 9:
         return redirect('/dashboard/admin')
 
     return redirect('/dashboard')
 
 def login_user(request):
-    # update later
-    print(request.POST['email'])
-    print(request.POST['password'])
-    return redirect('/signin')
+    user = User.objects.filter(email=request.POST['email'])
+    if not user:
+        request.session['failure'] = "No email/password combo" 
+        return redirect('/signin')
+    else:
+        if bcrypt.checkpw(request.POST['password'].encode(), user[0].password_hash.encode()):
+            if user[0].user_level == 9:
+                return redirect('/dashboard/admin')
+            else:
+                return redirect('/dashboard')
+        else:
+            request.session['failure'] = "No email/password combo" 
+            return redirect('/signin')
 
